6437ca7b8ecf50dec6ae1959024229a7cfb89a52
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2026-01-15 15:42:12 -0500

    question 2 done

--- class1Expercise/js/script2.js
+++ class1Expercise/js/script2.js
@@ -1,17 +1,5 @@
 "use strict";
 
-let dataEllipse = {
-    x:50,
-    y:50,
-    w:80,
-    h:80,
-
-    fill:{
-        r:175,
-        g:175,
-        b:175
-    }
-}
 /**
  * Create the canvas
 */
@@ -21,23 +9,14 @@ function setup() {
 
 function draw() {
     background("#87ceeb");
-    drawEllipse();
+    drawEllipse(50,50,80,80,175,175,175);
+    drawEllipse(70,120,100,100,100,100,100);
+    drawEllipse(90,180,120,120,30,45,60);
 }
 function drawEllipse(x,y,w,h,r,g,b){
     push();
     noStroke();
-    fill(175,175,175);
-    ellipse(50,50,80,80);
-    pop();
-
-    push()
-    fill(dataEllipse.fill.r-75,dataEllipse.fill.g-75, dataEllipse.fill.b-75);
-    ellipse(dataEllipse.x+20,dataEllipse.y+80,dataEllipse.w+40,dataEllipse.h+50);
+    fill(r,g,b);
+    ellipse(x,y,w,h);
     pop()
-
-    push()
-    fill(dataEllipse.fill.r-125,dataEllipse.fill.g-125, dataEllipse.fill.b-125);
-    ellipse(dataEllipse.x+60,dataEllipse.y+120,dataEllipse.w+10,dataEllipse.h+150);
-    pop()
-
 }
