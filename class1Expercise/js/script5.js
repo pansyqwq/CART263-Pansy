cfb42d5f653c84b8737b6cb62859bad2b15cbd41
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2026-01-18 18:47:38 -0500

    Update script5.js

--- class1Expercise/js/script5.js
+++ class1Expercise/js/script5.js
@@ -1,6 +1,14 @@
 "use strict";
 let counter = 0;
 
+let squareData = {
+    x: 25,
+    y: 25,
+    w: 50,
+    h: 50,
+    fill: "#f0a85cff",
+}
+
 /**
  * Create the canvas
 */
@@ -9,4 +17,32 @@ function setup() {
 }
 
 function draw() {
+    background("#2b2b2b");
+    displaySquare();
+    mouseHover();
+    console.log(counter);
+}
+
+function displaySquare() {
+    push();
+    noStroke();
+    fill(squareData.fill);
+    rect(squareData.x, squareData.y, squareData.w, squareData.h);
+    pop();
+}
+
+function mouseHover() {
+    if (mouseX >= 25 && mouseX <= 75 && mouseY >= 25 && mouseY <= 75) {
+        squareData.fill = "#f3e8b0ff";
+    } else {
+        squareData.fill = "#f0a85cff";
+    }
+}
+
+function mousePressed() {
+    if (mouseX >= 25 && mouseX <= 75 && mouseY >= 25 && mouseY <= 75) {
+        if (counter <= 9) {
+            counter += 1;
+        }
+    }
 }
