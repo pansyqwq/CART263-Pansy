3dd3aa349f065a0824a592695798a74201ea460c
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2026-01-18 20:44:14 -0500

    finish question 5 with the bonus

--- class1Expercise/js/script5.js
+++ class1Expercise/js/script5.js
@@ -9,6 +9,17 @@ let squareData = {
     fill: "#f0a85cff",
 }
 
+let squareData2 = {
+    x: 90,
+    y: 25,
+    w: 50,
+    h: 50,
+    fill: "#f0755cff",
+}
+let circleData = {
+    radious: 0,
+    fill: "#bebebe4a"
+}
 /**
  * Create the canvas
 */
@@ -19,7 +30,9 @@ function setup() {
 function draw() {
     background("#2b2b2b");
     displaySquare();
+    displaySquare2();
     mouseHover();
+    drawEllipse();
     console.log(counter);
 }
 
@@ -31,12 +44,38 @@ function displaySquare() {
     pop();
 }
 
+function displaySquare2() {
+    push();
+    noStroke();
+    fill(squareData2.fill);
+    rect(squareData2.x, squareData2.y, squareData2.w, squareData2.h);
+    pop();
+}
+
+function drawEllipse() {
+    noStroke();
+    fill(circleData.fill);
+
+    let i = 0;
+    while(i < counter){
+         ellipse(width / 2, height / 2, circleData.radious + i*25);
+         i++;
+    }
+}
+
 function mouseHover() {
     if (mouseX >= 25 && mouseX <= 75 && mouseY >= 25 && mouseY <= 75) {
         squareData.fill = "#f3e8b0ff";
     } else {
         squareData.fill = "#f0a85cff";
     }
+
+    if (mouseX >= 90 && mouseX <= 140 && mouseY >= 25 && mouseY <= 75) {
+        squareData2.fill = "#f3bdb0ff";
+    } else {
+        squareData2.fill = "#f0755cff";
+    }
+
 }
 
 function mousePressed() {
@@ -44,5 +83,11 @@ function mousePressed() {
         if (counter <= 9) {
             counter += 1;
         }
+    } else if (mouseX >= 90 && mouseX <= 140 && mouseY >= 25 && mouseY <= 75){
+        if (counter > 0) {
+            counter -= 1;
+        }
     }
 }
+
+
