f37dc4b404ad322d9f808d09d754fd2684aa2201
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2026-01-22 14:23:00 -0500

    updated html

--- class2Exercise/js/1_helloWorld.js
+++ class2Exercise/js/1_helloWorld.js
@@ -1,2 +1,8 @@
-// h2 means header 2, since we r using it in html, we need things like <h2>
-document.write("<h2>HELLO WORLD DYNAMIC</h2>");
+//controlling when to on load, here we r loading when the window appears
+window.onload = setup
+
+function setup(){
+    console.log("running setup");
+    // h2 means header 2, since we r using it in html, we need things like <h2>
+    document.write("HELLO WORLD AFTER LOAD IN FUNCTION");
+}
