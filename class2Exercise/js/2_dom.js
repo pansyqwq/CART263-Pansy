bf4e2fc562e1572ad23f3888a84064eb86560407
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2026-01-22 14:29:33 -0500

    new code inserted

--- class2Exercise/js/2_dom.js
+++ class2Exercise/js/2_dom.js
@@ -2,7 +2,7 @@
 window.onload = setup
 
 function setup(){
-    console.log("running setup");
+    console.log(document.getElementById("one")); //printing the part of html code linked under this id
     // h2 means header 2, since we r using it in html, we need things like <h2>
-    document.write("HELLO WORLD AFTER LOAD IN FUNCTION");
+//     document.write("HELLO WORLD AFTER LOAD IN FUNCTION");
 }
