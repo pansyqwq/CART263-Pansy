0d7326c7ef7bb6884165f439f944045182419031
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2026-01-29 15:48:59 -0500

    working

--- class3Exercise/js/move.js
+++ class3Exercise/js/move.js
@@ -14,7 +14,15 @@ window.onload = function () {
         let rect = this.getBoundingClientRect();
         let offsetX = e.clientX - rect.x;// getting the difference between the box and the actual coordinates
         let offsetY = e.clientY - rect.y;
-        drawBox.innerHTML = `x: ${offsetX}, y:${offsetY}`;
+        // drawBox.innerHTML = `x: ${offsetX}, y:${offsetY}`;
+
+
+        let pointDiv = document.createElement("div");
+        pointDiv.classList.add("point");
+        pointDiv.style.left = offsetX + "px";// adding the px string
+        pointDiv.style.top = offsetY + "px";
+        this.appendChild(pointDiv); // we are adding this new pointDiv as the child of the drawbox
+
     }
 
 
