12bdd8ddf48e2f7794c48b87088a6150f8d3c7ed
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2026-01-29 15:07:24 -0500

    done in class exercise

--- class3Exercise/js/3_events.js
+++ class3Exercise/js/3_events.js
@@ -37,18 +37,33 @@ function setup() {
   Selection.forEach(element => {
     element.addEventListener("click", function (e) { // the call back function
       if (this.getAttribute("custom-bool") === "inactive") {
-        this.setAttribute("custom-bool","active"); // no longer trigger it after the first click
+        this.setAttribute("custom-bool", "active"); // no longer trigger it after the first click
         console.log("is inactive");
         console.log(this.id)
         this.style.opacity = 0.5;// the opacity including the text will decrease
       }
       else {
-        this.setAttribute("custom-bool","inactive");
-         this.style.opacity = 1.0;
+        this.setAttribute("custom-bool", "inactive");
+        this.style.opacity = 1.0;
       }
-     
+
     });
+
+    document.querySelector("#bubbleButton").addEventListener("click", function () { // the e is optional
+      console.log("button clicked");
+      let bubble = document.createElement("div");
+      bubble.classList.add("bubble"); // adding a new class to bubble
+      bubble.style.left = `${Math.random() * (window.innerWidth - 200)}px`; //add px 
+      bubble.style.top = `${Math.random() * (window.innerHeight - 200)}px`; // we r calling this js function inside $ and multiply with a number
+
+      let r = Math.ceil(Math.random() * 255); //new Math.ceil
+      let g = Math.ceil(Math.random() * 255);// .ceil means the ceiling of the value
+      let b = Math.ceil(Math.random() * 255);
+
+
+      bubble.style.background = `rgba(${r},${g},${b})`;
+      document.getElementById("top-layer").appendChild(bubble)// we r taking a new div as a child element of the bubble
+    })
+
   });
-
-
 }
