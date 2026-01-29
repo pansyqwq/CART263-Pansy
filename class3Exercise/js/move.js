a5281f824f305e969084f732cb18c078a8d21519
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2026-01-29 15:37:39 -0500

    Update move.js

--- class3Exercise/js/move.js
+++ class3Exercise/js/move.js
@@ -1,4 +1,21 @@
 
-window.onload = function(){
-    console.log("move");
-}
+window.onload = function () {
+    // we want to do something when the mouse is over the box :)
+    let drawBox = document.querySelector("#draw-box-a");
+
+    //A: add event listener + callback
+    drawBox.addEventListener("mousemove", moveCallBack);
+
+    function moveCallBack(e) {
+        // console.log("mouse move");
+        // console.log(this);
+        // console.log(e.target);
+
+        let rect = this.getBoundingClientRect();
+        let offsetX = e.clientX - rect.x;// getting the difference between the box and the actual coordinates
+        let offsetY = e.clientY - rect.y;
+        drawBox.innerHTML = `x: ${offsetX}, y:${offsetY}`;
+    }
+
+
+};
