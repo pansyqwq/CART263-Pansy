d4a6dc4f7f049ba5f1de3a0755f9bd23847c1b1f
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2026-03-19 14:49:28 -0400

    started array project

---
+++ ARRAYS-CART263-START/js/arrayStuff.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+  // FOR BUILDING THE EXAMPLES :)
+  const allRows = document.querySelectorAll(".flex-row");
+  let arrayOfShapes_One = [];
+  	//INIT ONE:
+	//INIT TWO:
+	//INIT THREE
+  	//INIT FOUR
+  
+}
--- JsonExercise/js/jsonOne.js
+++ JsonExercise/js/jsonOne.js
@@ -1,34 +1,16 @@
 window.onload = goFetch;
 async function goFetch() { //async function allows multiple function running on the same time
-  // try {//try what ever is in here
-  //   let response = await fetch('../JsonExercise/files/tests.json'); //if we use fetch, it will return error if it doesn't run. fetch does, connect to internet and access this file and store it in json file
+  try {//try what ever is in here
+    let response = await fetch('../JsonExercise/files/tests.json'); //if we use fetch, it will return error if it doesn't run. fetch does, connect to internet and access this file and store it in json file
 
-  //   let parsedResultJS = await response.json();
-  //   console.log(parsedResultJS)//using async and await, make the loading of the link working on the back ground so it won't block everything else
-  //   displayResults(parsedResultJS)
-  // }
-  // catch (err) { //catch the error, if there's an error
-  //   console.log(err)//like a call back function
-  // }
-  try{
-
-    let urlA = `https://cataas.com/cat/gif/says/Hello?filter=mono&fontColor=orange&fontSize=20&type=square&json=true`
-    let urlB = `https://cataas.com/cat?json=true`
-    //
-    let response = await fetch(urlB) //response
-    let cat = await response.json();
-    console.log(cat)
-    displayOnSite(cat.url)
-
-    }
-     catch (err) {
-     console.log(err);
- }
-
- function displayOnSite(path){
-    console.log(path)
-    document.querySelector("#output_rev").innerHTML += `<img style = "width:90%"src= ${path} />`
- }
+    let parsedResultJS = await response.json();
+    console.log(parsedResultJS)//using async and await, make the loading of the link working on the back ground so it won't block everything else
+    displayResults(parsedResultJS)
+  }
+  catch (err) { //catch the error, if there's an error
+    console.log(err)//like a call back function
+  }
+ 
 }
 
 function displayResults(parsedResultJS) {
@@ -53,3 +35,31 @@ function displayResults(parsedResultJS) {
   }
 
 }
+let donut_as_json = {
+    "id": "0001",
+    "type": "donut",
+    "name": "Cake",
+    "image": "../images/donuts/b.png",
+    "flavours": [
+      { "id": "1001", "type": "Regular" },
+      { "id": "1002", "type": "Chocolate" },
+      { "id": "1003", "type": "Blueberry" },
+      { "id": "1004", "type": "Devil's Food" },
+    ],
+
+    "toppings": [
+      { "id": "5001", "type": "None" },
+      { "id": "5002", "type": "Glazed" },
+      { "id": "5005", "type": "Sugar" },
+      { "id": "5007", "type": "Powdered Sugar" },
+      { "id": "5006", "type": "Chocolate with Sprinkles" },
+      { "id": "5003", "type": "Chocolate" },
+      { "id": "5004", "type": "Maple" },
+    ],
+  };
+
+  let headingTag = document.createElement("h2");
+  headingTag.classList.add("donut_name_class");
+  document.getElementById("output_rev").appendChild(headingTag);
+  headingTag.innerHTML = `Donut Name : ${donut_as_json.name}`;
+  
