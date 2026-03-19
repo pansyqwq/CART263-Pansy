e60aeb583034587703341257de41c462cac68ceb
pansyqwq <138616481+pansyqwq@users.noreply.github.com>
2026-03-19 14:43:11 -0400

    cats images working

--- JsonExercise/js/jsonOne.js
+++ JsonExercise/js/jsonOne.js
@@ -1,15 +1,34 @@
 window.onload = goFetch;
 async function goFetch() { //async function allows multiple function running on the same time
-  try {//try what ever is in here
-    let response = await fetch('../JsonExercise/files/tests.json'); //if we use fetch, it will return error if it doesn't run. fetch does, connect to internet and access this file and store it in json file
+  // try {//try what ever is in here
+  //   let response = await fetch('../JsonExercise/files/tests.json'); //if we use fetch, it will return error if it doesn't run. fetch does, connect to internet and access this file and store it in json file
 
-    let parsedResultJS = await response.json();
-    console.log(parsedResultJS)//using async and await, make the loading of the link working on the back ground so it won't block everything else
-      displayResults(parsedResultJS)
-  }
-  catch (err) { //catch the error, if there's an error
-    console.log(err)//like a call back function
-  }
+  //   let parsedResultJS = await response.json();
+  //   console.log(parsedResultJS)//using async and await, make the loading of the link working on the back ground so it won't block everything else
+  //   displayResults(parsedResultJS)
+  // }
+  // catch (err) { //catch the error, if there's an error
+  //   console.log(err)//like a call back function
+  // }
+  try{
+
+    let urlA = `https://cataas.com/cat/gif/says/Hello?filter=mono&fontColor=orange&fontSize=20&type=square&json=true`
+    let urlB = `https://cataas.com/cat?json=true`
+    //
+    let response = await fetch(urlB) //response
+    let cat = await response.json();
+    console.log(cat)
+    displayOnSite(cat.url)
+
+    }
+     catch (err) {
+     console.log(err);
+ }
+
+ function displayOnSite(path){
+    console.log(path)
+    document.querySelector("#output_rev").innerHTML += `<img style = "width:90%"src= ${path} />`
+ }
 }
 
 function displayResults(parsedResultJS) {
